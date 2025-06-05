39646817ca5f12695e4b2c4a4c942bdb5e050aec
Wilson-Rodriguez <wilsonalex2002@gmail.com>
2025-06-05 17:37:05 -0600

    Add function to manage main game loop Created PlayRound() to add points to the score of whoever won last round. This determines the winner of a round.

--- default.js
+++ default.js
@@ -1,17 +1,74 @@
 console.log('banana')
-let rpsChoices = ['Rock', 'Paper', 'Scissor']
+let rpsChoices = ['rock', 'paper', 'scissor']
+
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice() {
     let number = Math.random()
     if (number >= 0 && number <= 0.3333) {
-        console.log(rpsChoices[0])
+        return rpsChoices[0]
     }
     else if (number > 0.3333 && number <= 0.6666) {
-        console.log(rpsChoices[1])
+        return rpsChoices[1]
     }
     else {
-        console.log(rpsChoices[2])
+        return rpsChoices[2]
     }
 }
 
-getComputerChoice()
+function getHumanChoice() {
+    return prompt()
+}
+
+function playRound(compChoice, humanChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    if(compChoice == 'rock') {
+        if (humanChoice = 'rock') {
+            console.log('You Tie')
+        }
+        else if (humanChoice = 'paper') {
+            console.log('You Win')
+            humanScore++
+        }
+        else if (humanChoice = 'scissor') {
+            console.log('You Lose')
+            computerScore++
+        }
+    }
+
+    else if (compChoice == 'paper') {
+        if (humanChoice = 'rock') {
+            console.log('You Lose')
+            computerScore++
+        }
+        else if (humanChoice == 'paper') {
+            console.log('You Tie')
+        }
+        else if (humanChoice == 'scissor') {
+            console.log('You Win')
+            humanchoice++
+        }
+    }
+
+    else if (compChoice == 'scissor') {
+        if (humanChoice = 'rock') {
+            console.log('You Win')
+            humanScore++
+        }
+        else if (humanChoice == 'paper') {
+            console.log('You Lose')
+            computerScore++
+        }
+        else if (humanChoice == 'scissor') {
+            console.log('You Tie')
+            humanchoice++
+        }
+    }
+
+    console.log(`Human Score: ${humanScore}`)
+    console.log(`Computer Score: ${computerScore}`)
+}
+const computerChoice = getComputerChoice()
+const humanChoice = getHumanChoice()
+playRound(computerChoice, humanChoice)
